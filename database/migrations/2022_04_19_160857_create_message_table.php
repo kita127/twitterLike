724a01e2594e8068d43198c79309e2008206274c
kita127 <kitada.kosuke@gmail.com>
2022-04-20 01:27:02 +0900

    messagesテーブル作成マイグレーション作成

---
+++ database/migrations/2022_04_19_160857_create_message_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->string('message');
+            $table->integer('favorite');
+            $table->string('type');
+            $table->bigInteger('message_id');
+            $table->bigInteger('norify_id');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+};
