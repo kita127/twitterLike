8dd9b446004d699d6564a67dd2aa3d0eaaf819ef
kita127 <kitada.kosuke@gmail.com>
2022-04-20 02:25:47 +0900

    tweetページ作成

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,6 +12,12 @@
                     You're logged in!
                 </div>
             </div>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <a href="/tweet">tweet</a>
+                </div>
+            </div>
+
         </div>
     </div>
 </x-app-layout>
