3419df6d7d9a0e9871f5718c1d7f9c8d19f82e5b
kita127 <kitada.kosuke@gmail.com>
2022-04-20 10:56:00 +0900

    ツイート機能簡易実装

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Tweet;
+use App\Models\Message;
 
 class TweetController extends Controller
 {
@@ -13,8 +13,18 @@ class TweetController extends Controller
         return view('tweet/index');
     }
 
-    public function store()
+    public function store(Request $request)
     {
+
+        $newMessage = new Message();
+        $newMessage->user_id = 0;
+        $newMessage->message = $request->message;
+        $newMessage->favorite = 0;
+        $newMessage->type = 'tweet';
+        $newMessage->message_id = -1;
+        $newMessage->norify_id = -1;
+        $newMessage->image = '';
+        $newMessage->save();
         return view('dashboard');
     }
 }
