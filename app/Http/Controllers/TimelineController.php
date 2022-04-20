2ad48ec70ab8a2c73a805deb7efda806c34ff985
kita127 <kitada.kosuke@gmail.com>
2022-04-20 11:43:11 +0900

    タイムラインに自分のメッセージが表示されるように変更

--- app/Http/Controllers/TimelineController.php
+++ app/Http/Controllers/TimelineController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Message;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class TimelineController extends Controller
 {
@@ -11,6 +13,12 @@ class TimelineController extends Controller
     public function index()
     {
         $messages = Message::all();
+
+        $messages = $messages->filter(function ($val) {
+            // 現在ユーザーのID
+            $user_id = Auth::id();
+            return $val->user_id == $user_id;
+        });
 
         return view('timeline/index', compact('messages'));
     }
