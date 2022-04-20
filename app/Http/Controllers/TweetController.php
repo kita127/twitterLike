a7c8d99b25d63a0dd4d5de8bb8cee728e855f7b2
kita127 <kitada.kosuke@gmail.com>
2022-04-20 22:41:42 +0900

    リプライ機能追加

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 use App\Models\Message;
+use App\Models\Norify;
 use Illuminate\Support\Facades\Auth;
 
 class TweetController extends Controller
@@ -28,6 +30,44 @@ class TweetController extends Controller
         $newMessage->norify_id = -1;
         $newMessage->image = '';
         $newMessage->save();
+
+
+        // リプライがある場合作成する
+        $to_replays = $this->getReplys($newMessage->message);
+        if (count($to_replays) > 0) {
+            foreach ($to_replays as $to_replay) {
+                $newNorify = new Norify();
+                $newNorify->to_user_id = $this->get_userid_by_name($to_replay);
+                $newNorify->from_user_id = $user_id;
+                $newNorify->message_id = $newMessage->id;
+                $newNorify->save();
+            }
+        }
+
         return view('dashboard');
+    }
+
+    private function get_userid_by_name($user_name)
+    {
+        return User::where('name', '=', $user_name)->get()->first()->id;
+    }
+
+    private function getReplys($str)
+    {
+        $all_users = User::all();
+        $all_user_names = $all_users->map(function ($var) {
+            return $var->name;
+        });
+
+        $to_replays = array();
+
+        foreach ($all_user_names as $un) {
+            if (strpos($str, '@' . $un) !== false) {
+                // 含まれる場合
+                array_push($to_replays, $un);
+            }
+        }
+
+        return $to_replays;
     }
 }
---
+++ database/migrations/2022_04_20_132914_create_norifies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('norifies', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('to_user_id');
+            $table->bigInteger('from_user_id');
+            $table->bigInteger('message_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('norifies');
+    }
+};
