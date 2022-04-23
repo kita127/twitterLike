d3d3e4adadf935209483e51d2f8f7a32933d4c64
kita127 <kitada.kosuke@gmail.com>
2022-04-23 12:00:07 +0900

    不要なコメント修正

--- resources/views/timeline/index.blade.php
+++ resources/views/timeline/index.blade.php
@@ -75,57 +75,6 @@
                 </ul>
             </div>
             @endforeach
-            <!--
-            <div class="row">
-                <div class="col-md-11 col-md-offset-1">
-                    <table class="table text-center">
-                        <tr>
-                            <th class="text-center">ユーザー</th>
-                            <th class="text-center">メッセージ内容</th>
-                            <th class="text-center">いいね数</th>
-                            <th class="text-center">いいねボタン</th>
-                            <th class="text-center">リツイートボタン</th>
-                            <th class="text-center">引用リツイート</th>
-                            <th class="text-center">画像</th>
-                        </tr>
-                        @foreach($message_and_retweet as $message)
-                        <tr>
-                            <td>{{ $message->name }}さん</td>
-                            <td>{{ $message->message }}</td>
-                            <td>{{ $message->favorite }}</td>
-                            <td>
-                                <form action="/favorite" method="post">
-                                    @csrf
-                                    <input type="hidden" name="message_id" value="{{ $message->id }}">
-                                    <button type="submit" class="btn btn-default">いいね！</button>
-                                </form>
-                            </td>
-                            <td>
-                                <form action="/retweet" method="post">
-                                    @csrf
-                                    <input type="hidden" name="message_id" value="{{ $message->id }}">
-                                    <button type="submit" class="btn btn-default">リツイート</button>
-                                </form>
-                            </td>
-                            <td>
-                                <form action="/refretweet" method="post">
-                                    @csrf
-                                    <input type="hidden" name="message_id" value="{{ $message->id }}">
-                                    <input type="text" name="message">
-                                    <button type="submit" class="btn btn-default">引用リツイート</button>
-                                </form>
-                            </td>
-                            <td>
-                                @if($message->image != '')
-                                <img src="{{ asset('storage/' . $message->image ) }}" class="w-48 ">
-                                @endif
-                            </td>
-                        </tr>
-                        @endforeach
-                    </table>
-                </div>
-            </div>
-            -->
         </div>
     </div>
 </x-app-layout>
