b75aa5bc86b476cb1f9622a95c8a8770862e3b51
kita127 <kitada.kosuke@gmail.com>
2022-04-24 10:32:11 +0900

    シード修正

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,6 @@ class DatabaseSeeder extends Seeder
         $this->call(MessageTableSeeder::class);
         $this->call(FollowTableSeeder::class);
         $this->call(UsersTableSeeder::class);
+        $this->call(NorifiesTableSeeder::class);
     }
 }
